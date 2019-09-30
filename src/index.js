ea255a7f54e1ca329bb9cb08f2f0f418b796585e
Karyuhin Sergej <karyuhin.sergej@gmail.com>
2019-09-30 11:15:07 +0300

    feat: test 16/20

--- src/index.js
+++ src/index.js
@@ -1,3 +1,105 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = str.split('');
+  let counter = 0;
+  let stack = [];
+  let special_bracket = 0;
+  let n = 0;
+
+  for (let i = 0; i < brackets.length; i++) {
+    if (brackets[i] == "|") {
+      special_bracket++;
+    }
+  }
+
+  if (special_bracket !== 0) {
+  if (special_bracket % 2 == 0) {
+    n = special_bracket / 2;
+    for (let i = 0; i < brackets.length; i++) {
+      if (brackets[i] == "|") {
+        if (n > 0) {
+          brackets[i] = "open";
+          n--;
+        }
+        else {
+          brackets[i] = "closed";
+        }
+      }
+    }
+  }
+  else {
+    return false;
+  }
+  }
+
+  for (let i = 0; i < brackets.length; i++) {
+    if (brackets[i] == "(" || brackets[i] == "["
+    || brackets[i] == "{" || brackets[i] == "open") {
+      counter++;
+      stack.push(brackets[i]);
+    }
+    else {
+
+      switch (brackets[i]) {
+
+        case ")":
+          if (stack[stack.length - 1] == "(") {
+            counter--;
+            stack.pop();
+            if (counter < 0) {
+              return false;
+            }
+          }
+          else {
+            return false;
+          }
+          break;
+
+        case "]":
+          if (stack[stack.length - 1] == "[") {
+            counter--;
+            stack.pop();
+            if (counter < 0) {
+              return false;
+            }
+          }
+          else {
+            return false;
+          }
+          break;
+
+        case "}":
+          if (stack[stack.length - 1] == "{") {
+            counter--;
+            stack.pop();
+            if (counter < 0) {
+              return false;
+            }
+          }
+          else {
+            return false;
+          }
+          break;
+
+          case "closed":
+            if (stack[stack.length - 1] == "open") {
+              counter--;
+              stack.pop();
+              if (counter < 0) {
+                return false;
+              }
+            }
+            else {
+              return false;
+            }
+            break;
+    }
+  }
+}
+
+  if (counter == 0) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
